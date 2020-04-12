f76122df53fd42de7372d0c308229cb7f0d93d18
Robert Arkenklo <github@arkenklo.net>
2020-04-12 23:19:46 +0200

    Update build_and_push.sh

--- build_and_push.sh
+++ build_and_push.sh
@@ -2,12 +2,9 @@
 
 set -e
 
-if [ -d public/ ]; then
-    rm -R public/
-fi
-hugo -D
+./build.sh
 
 git reset --soft # Reset git, don't touch any changes
 git add public/
 git commit -m "Hugo build at $(date +'%H:%M %d %B %Y')"
-#git push
+git push
